d2859e192fa7a98b74286ff4ef7f45d6ba69547a
Aldi Alimucaj <aldi.alimucaj@gmail.com>
2019-03-08 11:55:37 +0100

    [FEATURE] default home view

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Home: React.FunctionComponent<{}> = ({ children }) => {
+    return (
+        <div style={{ padding: 24, background: '#fff', minHeight: "90%" }}>
+            Home page
+        </div>
+    );
+};
+
+export default Home;
