12eb5c5aa5b134e8d11781660f075c5dd954aae5
Philipp Bauch <philippbauch15@gmail.com>
2019-03-10 20:41:32 +0100

    [REFACTOR] Update projects overview layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
-import { Button, Card, Col, Divider, Row } from 'antd';
+import { Card, Col, Divider, Icon, Row } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { getProjects } from '../../api/project.api';
 import { Project } from '../../models/Project';
 import './Home.scss';
 
-const Home: React.FunctionComponent<{}> = () => {
+const Home: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
   const [projects, setProjects] = useState<Project[]>(new Array<Project>());
   const fetchData = async () => {
     try {
@@ -19,18 +19,40 @@ const Home: React.FunctionComponent<{}> = () => {
     fetchData();
   }, []);
 
+  const selectProject = (id: string) => {
+    history.push(`/projects/${id}`);
+  };
+
   return (
-    <Card title="Projects" extra={<Link to="/projects/new">New Project</Link>}>
-      {projects.map((project: Project, index: number) => (
-        <Card.Grid>
-          <Link to={`/projects/${project._id}`}>
-            <h3>{project.title}</h3>
-            <p style={{ marginBottom: 0 }}>{project.description}</p>
-          </Link>
-        </Card.Grid>
-      ))}
-    </Card>
+    <React.Fragment>
+      <Row
+        type="flex"
+        justify="space-between"
+        align="middle"
+        style={{ marginBottom: 24 }}
+      >
+        <Col>
+          <h2 style={{ marginBottom: 0 }}>Projects</h2>
+        </Col>
+        <Col>
+          <Link to="/projects/new">New Project</Link>
+        </Col>
+      </Row>
+      <Divider />
+      <Row gutter={16}>
+        {projects.map((project: Project, index: number) => (
+          <Col span={6} key={index}>
+            <Link to={`/projects/${project._id}`}>
+              <Card className="clickable-card">
+                <h3>{project.title}</h3>
+                <p style={{ marginBottom: 0 }}>{project.description}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </React.Fragment>
   );
 };
 
-export default Home;
+export default withRouter(Home);
