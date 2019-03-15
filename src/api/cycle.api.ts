353fafa12ae97739856c7b640fce573db4a1a9fe
Philipp Bauch <philipp.bauch@kinexon.com>
2019-03-15 12:59:22 +0100

    [FEATURE] Cycle API

---
+++ src/api/cycle.api.ts
@@ -0,0 +1,53 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import {
+  ProjectAction,
+  setTestCases,
+} from '../contexts/ProjectContext/ProjectActions';
+import { Cycle } from '../models/Cycle';
+import agent from './agent';
+
+export function postCycle(cycle: Cycle) {
+  return agent
+    .post('/cycles', cycle)
+    .catch(
+      (error: AxiosError): AxiosResponse => {
+        const { message, response } = error;
+        if (!response) {
+          throw message;
+        }
+        return response;
+      }
+    )
+    .then(
+      (response: AxiosResponse): string => {
+        const { data, status, statusText } = response;
+        if (status !== 201 || !data.ok) {
+          throw statusText;
+        }
+        return data._id as string;
+      }
+    );
+}
+
+export function getCycles(): Promise<Cycle[]> {
+  return agent
+    .get('/cycles')
+    .catch(
+      (error: AxiosError): AxiosResponse => {
+        const { message, response } = error;
+        if (!response) {
+          throw message;
+        }
+        return response;
+      }
+    )
+    .then(
+      (response: AxiosResponse): Cycle[] => {
+        const { data, status, statusText } = response;
+        if (status !== 200) {
+          throw statusText;
+        }
+        return data as Cycle[];
+      }
+    );
+}
