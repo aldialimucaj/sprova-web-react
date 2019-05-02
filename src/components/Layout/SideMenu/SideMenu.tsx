99522a951e7d5d457624a2cd47b2f564632975ce
Philipp Bauch <philippbauch15@gmail.com>
2019-05-02 14:01:12 +0200

    [REFACTOR] Clean up SideMenu and remove cycle modal

--- src/components/Layout/SideMenu/SideMenu.tsx
+++ src/components/Layout/SideMenu/SideMenu.tsx
@@ -1,15 +1,10 @@
-import { postCycle } from '@/api/cycle.api';
-import { FormButton, FormInput, FormTextArea } from '@/components/form';
 import Menu, { Item, Section } from '@/components/Menu';
-import Modal from '@/components/Modal';
 import { CycleContext } from '@/contexts/CycleContext';
 import { ProjectContext } from '@/contexts/ProjectContext';
-import { useFormInput } from '@/hooks/useFormInput';
-import { useFormTextArea } from '@/hooks/useFormTextArea';
 import logo from '@/images/sprova.svg';
 import { Cycle } from '@/models/Cycle';
-import { Button, Divider, Form, Icon, notification, Select, Spin } from 'antd';
-import React, { Fragment, useContext, useState } from 'react';
+import { Button, Divider, Icon, Select, Spin } from 'antd';
+import React, { Fragment, useContext } from 'react';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import './SideMenu.scss';
 
@@ -18,174 +13,94 @@ const Option = Select.Option;
 const SideMenu: React.FunctionComponent<RouteComponentProps> = ({
   history,
 }) => {
-  const { currentCycle, cycles, isCyclesLoading, onAddCycle } = useContext(
-    CycleContext
-  );
+  const { currentCycle, cycles, isCyclesLoading } = useContext(CycleContext);
   const { currentProject, isProjectsLoading } = useContext(ProjectContext);
 
-  const { value: cycleTitle, setValue: setCycleTitle } = useFormInput('');
-  const {
-    value: cycleDescription,
-    setValue: setCycleDescription,
-  } = useFormTextArea('');
-  const [isCycleModalOpen, setIsCycleModalOpen] = useState(false);
-  const [isCycleSubmitLoading, setIsCycleSubmitLoading] = useState(false);
-
-  const handleCycleSubmit = async (
-    event: React.FormEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-
-    const cycleNew: Partial<Cycle> = {
-      title: cycleTitle,
-      description: cycleDescription,
-      projectId: currentProject!._id,
-    };
-
-    setIsCycleSubmitLoading(true);
-
-    try {
-      const cycle = await postCycle(cycleNew);
-      onAddCycle(cycle);
-      setIsCycleModalOpen(false);
-      setCycleTitle('');
-      setCycleDescription('');
-      notification.success({
-        placement: 'bottomRight',
-        message: `${cycleTitle} created`,
-        description: `Cycle created with ID ${cycle._id}`,
-      });
-    } catch (error) {
-      notification.error({
-        placement: 'bottomRight',
-        message: 'Failed to create cycle',
-        description: error,
-      });
-    } finally {
-      setIsCycleSubmitLoading(false);
-    }
-  };
-
-  const cycleModal = (
-    <Modal
-      title="Create New Cycle"
-      open={isCycleModalOpen}
-      onClose={() => setIsCycleModalOpen(false)}
-    >
-      <Form layout="vertical" onSubmit={handleCycleSubmit}>
-        <FormInput
-          label="Title"
-          value={cycleTitle}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setCycleTitle(event.currentTarget.value)
-          }
-          placeholder="Cycle"
-          required={true}
-        />
-        <FormTextArea
-          value={cycleDescription}
-          onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
-            setCycleDescription(event.currentTarget.value)
-          }
-          label="Description"
-          placeholder="Description"
-          minLength={3}
-        />
-        <FormButton
-          style={{ marginBottom: 0, paddingBottom: 0 }}
-          type="primary"
-          loading={isCycleSubmitLoading}
-          disabled={!cycleTitle}
-        >
-          Create Cycle
-        </FormButton>
-      </Form>
-    </Modal>
-  );
-
   return (
-    <Fragment>
-      <div className="sprova-sidemenu">
-        <div
-          className="sprova-sidemenu-title"
-          onClick={() =>
-            currentProject && history.push(`/projects/${currentProject._id}`)
-          }
-        >
-          <img id="sprova-logo" src={logo} alt="logo" />
-          <h3 id="sprova-project-title">
-            {(currentProject && currentProject.title) || 'Sprova'}
-          </h3>
-        </div>
-
-        <div className="sprova-sidemenu-menu">
-          {isProjectsLoading || isCyclesLoading ? (
-            <Spin />
-          ) : currentProject ? (
-            <Fragment>
-              <div className="sprova-sidemenu-menu-section">Cycle</div>
-              {currentCycle ? (
-                <Select
-                  defaultValue={currentCycle._id}
-                  dropdownRender={(menu) => (
-                    <div>
-                      {menu}
-                      <Divider style={{ margin: '4px 0' }} />
-                      <div style={{ padding: '8px', cursor: 'pointer' }}>
-                        <Icon type="plus" /> Add item
-                      </div>
-                    </div>
-                  )}
-                >
-                  {cycles.map((cycle: Cycle) => (
-                    <Option key={cycle._id} value={cycle._id}>
-                      {cycle.title}
-                    </Option>
-                  ))}
-                </Select>
-              ) : (
-                <Button
-                  type="primary"
-                  block={true}
-                  onClick={() => setIsCycleModalOpen(true)}
-                  style={{ marginTop: 8 }}
-                >
-                  Create New Cycle
-                </Button>
-              )}
-
-              {currentCycle ? (
-                <Menu>
-                  <Section title="Menu">
-                    <Item icon={<Icon type="home" />}>
-                      <Link to={`/projects/${currentProject._id}`}>Home</Link>
-                    </Item>
-                    <Item icon={<Icon type="thunderbolt" />}>
-                      <Link to={`/projects/${currentProject._id}/executions`}>
-                        Executions
-                      </Link>
-                    </Item>
-                    <Item icon={<Icon type="file-text" />}>
-                      <Link to={`/projects/${currentProject._id}/testcases`}>
-                        Test Cases
-                      </Link>
-                    </Item>
-                  </Section>
-                  <Section title="More">
-                    <Item icon={<Icon type="setting" />}>
-                      <Link to={`/projects/${currentProject._id}/settings`}>
-                        Project Settings
-                      </Link>
-                    </Item>
-                  </Section>
-                </Menu>
-              ) : null}
-            </Fragment>
-          ) : null}
-        </div>
+    <div className="sprova-sidemenu">
+      <div
+        className="sprova-sidemenu-title"
+        onClick={() =>
+          currentProject && history.push(`/projects/${currentProject._id}`)
+        }
+      >
+        <img id="sprova-logo" src={logo} alt="logo" />
+        <h3 id="sprova-project-title">
+          {(currentProject && currentProject.title) || 'Sprova'}
+        </h3>
       </div>
-      {cycleModal}
-    </Fragment>
+
+      <div className="sprova-sidemenu-menu">
+        {isProjectsLoading || isCyclesLoading ? (
+          <Spin />
+        ) : currentProject ? (
+          <Menu>
+            <Section title="Cycle">
+              {currentCycle ? (
+                <Item>
+                  <Select
+                    defaultValue={currentCycle._id}
+                    dropdownRender={(menu) => (
+                      <div>
+                        {menu}
+                        <Divider style={{ margin: '4px 0' }} />
+                        <div style={{ padding: '8px', cursor: 'pointer' }}>
+                          <Icon type="plus" /> Add item
+                        </div>
+                      </div>
+                    )}
+                  >
+                    {cycles.map((cycle: Cycle) => (
+                      <Option key={cycle._id} value={cycle._id}>
+                        {cycle.title}
+                      </Option>
+                    ))}
+                  </Select>
+                </Item>
+              ) : (
+                <Item>
+                  <Link to={`/projects/${currentProject._id}/cycles/new`}>
+                    <Button
+                      type="primary"
+                      block={true}
+                      style={{ marginTop: 8 }}
+                    >
+                      Create New Cycle
+                    </Button>
+                  </Link>
+                </Item>
+              )}
+            </Section>
+            {currentCycle ? (
+              <Fragment>
+                <Section title="Menu">
+                  <Item icon={<Icon type="home" />}>
+                    <Link to={`/projects/${currentProject._id}`}>Home</Link>
+                  </Item>
+                  <Item icon={<Icon type="thunderbolt" />}>
+                    <Link to={`/projects/${currentProject._id}/executions`}>
+                      Executions
+                    </Link>
+                  </Item>
+                  <Item icon={<Icon type="file-text" />}>
+                    <Link to={`/projects/${currentProject._id}/testcases`}>
+                      Test Cases
+                    </Link>
+                  </Item>
+                </Section>
+                <Section title="More">
+                  <Item icon={<Icon type="setting" />}>
+                    <Link to={`/projects/${currentProject._id}/settings`}>
+                      Project Settings
+                    </Link>
+                  </Item>
+                </Section>
+              </Fragment>
+            ) : null}
+          </Menu>
+        ) : null}
+      </div>
+    </div>
   );
 };
 
