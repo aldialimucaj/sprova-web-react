cb33fcfa88cc446a15eef250baa89c306e682975
Philipp Bauch <philipp.bauch@kinexon.com>
2019-05-07 13:59:42 +0200

    [REFACTOR] ProjectDetails remove Modal

--- src/pages/Project/ProjectDetails/ProjectDetails.tsx
+++ src/pages/Project/ProjectDetails/ProjectDetails.tsx
@@ -1,26 +1,11 @@
-import { postCycle } from '@/api/cycle.api';
 import Card, { CardBody } from '@/components/Card';
-import { FormButton, FormInput, FormTextArea } from '@/components/form';
 import { PageContent, PageHeader } from '@/components/Layout';
 import Level from '@/components/Level';
-import Modal from '@/components/Modal';
 import { CycleContext } from '@/contexts/CycleContext';
 import { ProjectContext } from '@/contexts/ProjectContext';
-import { useFormInput } from '@/hooks/useFormInput';
-import { useFormTextArea } from '@/hooks/useFormTextArea';
 import { Cycle } from '@/models/Cycle';
-import {
-  Button,
-  Col,
-  Divider,
-  Form,
-  Icon,
-  notification,
-  Row,
-  Spin,
-  Tag,
-} from 'antd';
-import React, { Fragment, useContext, useState } from 'react';
+import { Button, Col, Divider, Icon, Row, Spin, Tag } from 'antd';
+import React, { Fragment, useContext } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 interface Params {
@@ -32,61 +17,21 @@ const ProjectDetails: React.FunctionComponent<RouteComponentProps<Params>> = ({
   history,
 }) => {
   const { currentProject } = useContext(ProjectContext);
-  const { cycles, isCyclesLoading } = useContext(CycleContext);
-
-  const { value: cycleTitle, setValue: setCycleTitle } = useFormInput('');
-  const {
-    value: cycleDescription,
-    setValue: setCycleDescription,
-  } = useFormTextArea('');
-  const [isCycleModalOpen, setIsCycleModalOpen] = useState(false);
-  const [isCycleSubmitLoading, setIsCycleSubmitLoading] = useState(false);
-
-  const handleCycleSubmit = async (
-    event: React.FormEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-
-    const cycleNew: Partial<Cycle> = {
-      title: cycleTitle,
-      description: cycleDescription,
-      projectId: currentProject!._id,
-    };
-
-    setIsCycleSubmitLoading(true);
-
-    try {
-      const cycle = await postCycle(cycleNew);
-      cycles!.push(cycle);
-      setIsCycleModalOpen(false);
-      notification.success({
-        placement: 'bottomRight',
-        message: `${cycleTitle} created`,
-        description: `Cycle created with ID ${cycle._id}`,
-      });
-    } catch (error) {
-      notification.error({
-        placement: 'bottomRight',
-        message: 'Failed to create cycle',
-        description: error,
-      });
-    } finally {
-      setIsCycleSubmitLoading(false);
-    }
-  };
+  const { currentCycle, cycles, isCyclesLoading } = useContext(CycleContext);
 
   return currentProject ? (
     <Fragment>
-      <PageHeader title={currentProject!.title} subTitle="Overview" />
+      <PageHeader
+        title={currentProject!.title}
+        subTitle={(currentCycle && currentCycle.title) || 'Overview'}
+      />
       <PageContent>
         <Level align="bottom">
           <div>
             <h3 style={{ display: 'inline-block', marginRight: 8 }}>Cycles</h3>
             <Icon style={{ cursor: 'pointer' }} type="info-circle" />
           </div>
-          <Button onClick={() => setIsCycleModalOpen(true)} type="primary">
-            New
-          </Button>
+          <Button type="primary">New</Button>
         </Level>
         <Divider />
         {!isCyclesLoading && cycles ? (
@@ -116,41 +61,6 @@ const ProjectDetails: React.FunctionComponent<RouteComponentProps<Params>> = ({
         ) : (
           <Spin />
         )}
-
-        <Modal
-          title="Create New Cycle"
-          open={isCycleModalOpen}
-          onClose={() => setIsCycleModalOpen(false)}
-        >
-          <Form layout="vertical" onSubmit={handleCycleSubmit}>
-            <FormInput
-              label="Title"
-              value={cycleTitle}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                setCycleTitle(event.currentTarget.value)
-              }
-              placeholder="Cycle"
-              required={true}
-            />
-            <FormTextArea
-              value={cycleDescription}
-              onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) =>
-                setCycleDescription(event.currentTarget.value)
-              }
-              label="Description"
-              placeholder="Description"
-              minLength={3}
-            />
-            <FormButton
-              style={{ marginBottom: 0, paddingBottom: 0 }}
-              type="primary"
-              loading={isCycleSubmitLoading}
-              disabled={!cycleTitle}
-            >
-              Create Cycle
-            </FormButton>
-          </Form>
-        </Modal>
       </PageContent>
     </Fragment>
   ) : null;
