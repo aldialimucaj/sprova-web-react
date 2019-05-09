94dfb7b9938860623973e7a56bd4bd31b3027591
Philipp Bauch <philippbauch15@gmail.com>
2019-05-09 14:03:43 +0200

    [TEST] Basic Card tests

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,3 +1,4 @@
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
 import sinon from 'sinon';
@@ -6,6 +7,13 @@ import Card, { CardBody, CardHeader } from './Card';
 describe('<Card />', () => {
   it('renders a `.sprova-card`', () => {
     const wrapper = shallow(<Card />);
-    expect(wrapper.find('.sprova-card')).toHaveLength(1);
+    expect(wrapper.find('.sprova-card')).to.have.lengthOf(1);
+  });
+
+  it('simulates click events', () => {
+    const handleClick = sinon.spy();
+    const wrapper = shallow(<Card onClick={handleClick} />);
+    wrapper.simulate('click');
+    expect(handleClick).to.have.property('callCount', 1);
   });
 });
